5f4c6d3e0f00d78a6f7dacfad510d18b706a3310
Vu Tien Dat <tiendatuet@gmail.com>
2022-10-03 14:17:26 +0700

    firsr commit

---
+++ README.md
@@ -0,0 +1,6 @@
+Navigation Components Example
+
+An example using a single activity, several fragments, animations, arguments, with Navigation Components (Kotlin)
+
+
+
---
+++ app/src/androidTest/java/com/tdsolo/navigationcomponentsexample/ExampleInstrumentedTest.kt
@@ -0,0 +1,20 @@
+package com.tdsolo.navigationcomponentsexample
+
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("com.tdsolo.navigationcomponentsexample", appContext.packageName)
+    }
+}
